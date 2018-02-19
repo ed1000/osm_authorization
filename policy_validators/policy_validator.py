0769b33318964298d46fa4fc1f3cdb4ce4dfdf5b
Eduardo Sousa <eduardosousa@av.it.pt>
2018-02-19 23:04:49 +0000

    Adding Redis requirement to txt

--- policy_validators/policy_validator.py
+++ policy_validators/policy_validator.py
@@ -1,21 +1,33 @@
-import re, json, pprint
-
+import re, json, pprint, redis, hashlib, time
 from settings import Config
 
 class ActionGrant:
-    def __init__(self, action_id, audit_id, action, project):
+    def __init__(self, action_id, action, project, subject, service, operations=[]):
         self.action_id = action_id
-        self.audit_id = audit_id
         self.action = action
         self.project = project
+        self.subject = subject
+        self.service = service
+        self.operations = operations
     
     def to_public_dict(self):
         data = dict()
 
         data['action_id'] = self.action_id
-        data['audit_id'] = self.audit_id
         data['action'] = self.action
         data['project'] = self.project
+
+        return data
+    
+    def to_redis_dict(self):
+        data = dict()
+
+        data['action_id'] = self.action_id
+        data['action'] = self.action
+        data['project'] = self.project
+        data['subject'] = self.subject
+        data['service'] = self.service
+        data['operations'] = self.operations
 
         return data
 
@@ -102,7 +114,6 @@ def validate_policy(policy_file):
 
 
 class PolicyValidator:
-    COMPILED_REGEX = re.compile(r'([ a-zA-Z0-9_]+)_[0-9]+')
     POLICIES = validate_policy(json.load(open(Config.AUTHORIZATION_POLICY_FILE)))
     EXPIRATION_TIME = Config.REDIS_EXPIRATION_TIME
     USER_MARKER = Config.AUTHORIZATION_USER_MARKER
@@ -113,37 +124,9 @@ class PolicyValidator:
         if PolicyValidator.POLICIES is None:
             raise Exception('POLICY FILE NOT VALID')
 
-        #self.redis_db = StrictRedis(host=settings.REDIS_URL, port=settings.REDIS_PORT)
-"""
-    def verify_issuer_permissions(self, call_chain, issuer, is_in_project=False):
-        markers = str.split(call_chain[0], '|')
-
-        if len(markers) == 1:
-            if PolicyValidator.USER_MARKER in markers:
-                return not issuer.is_service and not issuer.is_admin and is_in_project
-            elif PolicyValidator.SERVICE_MARKER in markers:
-                return issuer.is_service
-            elif PolicyValidator.ADMIN_MARKER in markers:
-                return issuer.is_admin
-        elif len(markers) == 2:
-            if PolicyValidator.USER_MARKER in markers and \
-                    PolicyValidator.ADMIN_MARKER in markers:
-                return issuer.is_admin or is_in_project
-            elif PolicyValidator.ADMIN_MARKER in markers and \
-                    PolicyValidator.SERVICE_MARKER in markers:
-                return issuer.is_admin or issuer.is_service
-
-        return False
-
-    def verify_component(self, call_chain, component, position):
-        search = re.search(PolicyValidator.COMPILED_REGEX, component.username)
-
-        if search:
-            return call_chain[position] == search.group(1)
-        return False
+        self.redis_db = redis.StrictRedis(host=Config.REDIS_URL, port=Config.REDIS_PORT)
 
     def generate_action_id(self, action):
-        return None
         action_id = hashlib.sha512(str.encode(action + str(time.time())))
 
         while self.redis_db.exists(action_id):
@@ -151,55 +134,60 @@ class PolicyValidator:
 
         return action_id
 
-    def verify_authorization(self, action, project, issuer, component, action_id=None):
-        return None
-        if not self.is_valid:
-            raise PolicyFileError("Authorization policy file has an error...")
-
-        call_chain = PolicyValidator.POLICIES.get(action)
-
-        if not call_chain:
-            return False
-
-        if action_id is None:
-            user_in_project = project in TokenValidator().get_project_list(issuer.token)
-
-            user_permission = self.verify_issuer_permissions(call_chain, issuer, user_in_project)
-            service_permission = self.verify_component(call_chain, component, 0)
-
-            if user_permission and service_permission:
-                action_id = self.generate_action_id(action=action)
-
-                self.redis_db.rpush(action_id, issuer.username)
-                self.redis_db.rpush(action_id, component.username)
-                self.redis_db.expire(action_id, PolicyValidator.EXPIRATION_TIME)
-
-                return True, action_id
-        else:
-            is_action_expired = self.redis_db.exists(action_id)
-
-            if not is_action_expired:
-                position = self.redis_db.llen(action_id)-1
-
-                caller_component_permission = self.verify_component(call_chain, issuer, position)
-                callee_component_permission = self.verify_component(call_chain, component, position + 1)
-                caller_previous_authorization = self.redis_db.lindex(action_id, -1) == issuer.username
-
-                if caller_component_permission and callee_component_permission and \
-                        caller_previous_authorization:
-                    self.redis_db.rpush(action_id, component.username)
-                    self.redis_db.expire(action_id, PolicyValidator.EXPIRATION_TIME)
-                    return True, action_id
-
-        return False,
-
-    
-    def __init__(self):
-        pass
-    
     def create_authorization(self, action, project, subject, service):
-        return ActionGrant(action_id=0, audit_id=0, action=action, project=project)
+        try:
+            if action not in PolicyValidator.POLICIES['actions']:
+                return None
 
-    def verify_authorization(self, action, project, client_service, server_service):
-        return ActionGrant(action_id=0, audit_id=0, action=action, project=project)
-"""
+            # Check if service is service
+
+            action_policy = filter(
+                lambda x: x['action_name'] == action, 
+                PolicyValidator.POLICIES['action_policies'])
+            
+            if len(action_policy) != 0:
+                pass
+                # Verify if subject/service match policy
+            
+            action_id = self.generate_action_id(action)
+
+            action_grant = ActionGrant(action_id=action_id, action=action, project=project).to_public_dict()
+
+            self.redis_db.hmset(action_id, action_grant)
+
+            return action_grant
+        except Exception as ex:
+            print(ex)
+
+            return None
+
+    def verify_authorization(self, action_id, action, operation, project, client_service, server_service):
+        try:
+            if action not in PolicyValidator.POLICIES['actions']:
+                return None
+            
+            if operation not in PolicyValidator.POLICIES['operations']:
+                return None
+            
+            action_to_operations = filter(
+                lambda x: x['action_name'] == action, 
+                PolicyValidator.POLICIES['action_to_operations'])
+
+            if operation not in action_to_operations:
+                return None
+            
+            action_policy = filter(
+                lambda x: x['action_name'] == action, 
+                PolicyValidator.POLICIES['action_policies'])
+
+            if len(action_policy) != 0:
+
+                pass
+
+            action_grant = ActionGrant(action_id=action_id, action=action, project=project).to_public_dict()
+
+            return action_grant
+        except Exception as ex:
+            print(ex)
+
+            return None
